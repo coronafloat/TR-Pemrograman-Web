f6f2104c117aa6760ea97c33d486264afb8aa377
Alvaro-10-Git <mikhaelalvarogmz10@gmail.com>
2024-11-25 11:43:04 +0700

    Update view-main-user.php

--- frontend/user/view-main-user.php
+++ frontend/user/view-main-user.php
@@ -10,6 +10,7 @@
 
 <body>
     <h3>Ini adalah halaman pertama yang muncul ketika masuk sebagai user</h3>
+    <h3>Disini akan dilakukan : Pilih jadwal kelas, pesan kursus, lihat detail kursus.</h3>
 </body>
 
 </html>
