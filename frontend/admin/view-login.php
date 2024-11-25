0aa0aae5d0285d9e690530e983f3fd06781495bc
Alvaro-10-Git <mikhaelalvarogmz10@gmail.com>
2024-11-25 11:41:43 +0700

    Update view-login.php

--- frontend/admin/view-login.php
+++ frontend/admin/view-login.php
@@ -1,29 +1,29 @@
 <?php
-session_start();
+    session_start();
 ?>
-<html>
+    <html>
 
-<head>
-    <title>login</title>
-</head>
+    <head>
+        <title>Login</title>
+    </head>
 
-<body>
-    <form method="post" action="../../backend/admin/login-proses.php">
-        ID:
-        <input type="text" name="id" required>
-        <br>
+    <body>
+        <form method="post" action="../../backend/admin/login-proses.php">
+            ID
+            <input type="text" name="id" required>
+            <br><br>
 
-        Nama:
-        <input type="text" name="nama" required>
-        <br>
+            Nama
+            <input type="text" name="nama" required>
+            <br><br>
 
-        Password:
-        <input type="password" name="password" required>
-        <br>
+            Password
+            <input type="password" name="password" required>
+            <br><br>
 
-        <!-- SUBMIT BUTTON -->
-        <input type="submit" value="Submit">
-    </form>
-</body>
+            <!-- SUBMIT BUTTON -->
+            <input type="submit" value="Submit">
+        </form>
+    </body>
 
-</html>
+    </html>
