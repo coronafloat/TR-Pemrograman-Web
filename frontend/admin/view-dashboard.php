3039f4a66a3a329464f7da8ca082cb5942a0eded
Alvaro-10-Git <mikhaelalvarogmz10@gmail.com>
2024-11-25 11:41:05 +0700

    Update view-dashboard.php

--- frontend/admin/view-dashboard.php
+++ frontend/admin/view-dashboard.php
@@ -1,28 +1,32 @@
 <?php
-session_start();
+    session_start();
 
-if (!isset($_SESSION["admin_logged_in"]) || $_SESSION["admin_logged_in"] == false) {
-    header("Location: ../../frontend/admin/view-login.php");
-}
-
+    if (!isset($_SESSION["admin_logged_in"]) || $_SESSION["admin_logged_in"] == false) {
+        header("Location: ../../frontend/admin/view-login.php");
+    }
 ?>
 
-<html>
+    <html>
 
-<head>
-    <title>Dashboard Admin</title>
-    <script>
-        function logoutPage() {
-            window.location.href = "../../backend/admin/logout.php";
-        }
-    </script>
-</head>
+    <head>
+        <title>Dashboard Admin</title>
+        <script>
+            function logoutPage() {
+                window.location.href = "../../backend/admin/logout.php";
+            }
 
-<body>
-    <h1>Selamat datang, <?php echo htmlspecialchars($_SESSION['admin_name']); ?>!</h1>
-    <p>disini akan dilakukan CRUD JADWAL sesuai dengan perintah TR</p>
-    <!-- <a href="../../backend/admin/logout.php">Logout</a> -->
-    <button onclick="logoutPage()">LOGOUT</button>
-</body>
+            function adminPage() {
+                window.location.href = "../../frontend/admin/view-tampil-kursus.php"
+            }
+        </script>
+    </head>
 
-</html>
+    <body>
+        <h1>Selamat Datang, <?php echo htmlspecialchars($_SESSION['admin_name']); ?>!</h1>
+        <p>Disini akan dilakukan : Tambah kursus, ubah jadwal kursus, hapus kursus, kelola jadwal. ( CRUD Jadwal )</p>
+        <!-- <a href="../../backend/admin/logout.php">Logout</a> -->
+        <button onclick="adminPage()">KELOLA JADWAL</button>
+        <button onclick="logoutPage()">LOGOUT</button>
+    </body>
+
+    </html>
