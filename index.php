175a47873daac4d41d6e2514265380ba4f612b76
Daniel Mahardhika <danielsatriamahardhiak@gmail.com>
2024-12-03 19:07:41 +0700

    betulin index.php

--- index.php
+++ index.php
@@ -8,32 +8,32 @@
     <title>ProRider</title>
 
     <!-- Menambahkan Favicon -->
-    <link rel="icon" href="../../assets/favicon-logo.png" type="image/png">
+    <link rel="icon" href="assets/favicon-logo.png" type="image/png">
     
     <!--TODO: function java script untuk mengarahkan ke halaman login atau sign up user -->
     <script>
         function adminPage() {
-            window.location.href = "../../frontend/admin/view-login.php";
+            window.location.href = "frontend/admin/view-login.php";
         }
 
         function userPage() {
-            window.location.href = "../../frontend/user/view-login-user.php";
+            window.location.href = "frontend/user/view-login-user.php";
         }
 
         function signUpPage() {
-            window.location.href = "../../frontend/user/view-signup-user.php";
+            window.location.href = "frontend/user/view-signup-user.php";
         }
 
         function gabungKursus() {
-            window.location.href = "../../frontend/user/view-create-user.php";
+            window.location.href = "frontend/user/view-create-user.php";
         }
 
         function learnMoreMobil() {
-            window.location.href = "../../frontend/user/layanan-mobil.php";
+            window.location.href = "frontend/user/layanan-mobil.php";
         }
 
         function learnMoreMotor() {
-            window.location.href = "../../frontend/user/layanan-motor.php";
+            window.location.href = "frontend/user/layanan-motor.php";
         }
     </script>
 
@@ -47,13 +47,13 @@
         <div class="max-w-screen-xl mx-auto px-6 flex items-center justify-between">
             <!-- Logo -->
             <div class="flex items-center">
-                <img src="../../assets/logo-prorider.png" alt="Logo" class="h-12">
+                <img src="assets/logo-prorider.png" alt="Logo" class="h-12">
             </div>
 
             <!-- Menu -->
             <div class="md:flex items-center space-x-6">
                 <a href="index.php" class="text-gray-700 font-medium hover:text-[#89A8B2]">Home</a>
-                <a href="../../frontend/user/about-us.php" class="text-gray-700 font-medium hover:text-[#89A8B2]">About</a>
+                <a href="frontend/user/about-us.php" class="text-gray-700 font-medium hover:text-[#89A8B2]">About</a>
                 <a href="#" onclick="window.open('https://wa.me/6287724061150?text=Hi, Saya ingin menghubungi Anda!', '_blank')" class="text-gray-700 font-medium hover:text-[#89A8B2]">Contact Us</a>
 
                 <!-- Button Login -->
@@ -69,7 +69,7 @@
     </nav>
 
     <!-- Banner content -->
-    <section class="bg-[url('../../assets/belajar-mobil3.jpg')] bg-cover bg-center bg-no-repeat mt-1/2">
+    <section class="bg-[url('assets/belajar-mobil3.jpg')] bg-cover bg-center bg-no-repeat mt-1/2">
       <div class="bg-black/60 p-8 md:p-12 lg:px-16 lg:py-24 flex items-center justify-center">
         <div class="flex items-center justify-center text-white flex-col">
           <h2 class="text-2xl font-bold text-white sm:text-3xl md:text-5xl">WELCOME MY RIDER!</h2>
@@ -95,7 +95,7 @@
 
       <!-- Card Mobil -->
       <div class="bg-white shadow-lg rounded-lg overflow-hidden w-64">
-        <img src="../../assets/belajar-mobil.jpg" alt="Mobil" class="w-full h-40 object-cover">
+        <img src="assets/belajar-mobil.jpg" alt="Mobil" class="w-full h-40 object-cover">
         <div class="p-4">
           <h3 class="text-xl font-semibold text-gray-800">Layanan Mobil</h3>
           <p class="text-gray-600 mt-2">
@@ -110,7 +110,7 @@
 
       <!-- Card Motor -->
       <div class="bg-white shadow-lg rounded-lg overflow-hidden w-64">
-        <img src="../../assets/kursus-motor.jpeg" alt="Motor" class="w-full h-40 object-cover">
+        <img src="assets/kursus-motor.jpeg" alt="Motor" class="w-full h-40 object-cover">
         <div class="p-4">
           <h3 class="text-xl font-semibold text-gray-800">Layanan Motor</h3>
           <p class="text-gray-600 mt-2">Ikuti kursus dan pelajari cara mengemudi motor dengan aman, menguasai teknik belok, rem, dan manuver lainnya dengan pengajaran langsung dari instruktur berlisensi.</p>
@@ -132,7 +132,7 @@
       <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
         <div class="sm:flex sm:items-center sm:justify-between">
           <div class="flex justify-center text-teal-600 sm:justify-start">
-            <img src="../../assets/logo-prorider.png" alt="Logo ProRider" class="h-12">
+            <img src="assets/logo-prorider.png" alt="Logo ProRider" class="h-12">
           </div>
 
           <p class="mt-4 text-center text-sm text-black lg:mt-0 lg:text-right">
